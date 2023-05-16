6c7a94c9833eb91d8967dc9a5b10173b282e4c73
hfnu_2010211112 <2942885033@qq.com>
2023-05-16 21:34:03 +0800

    init project

---
+++ src/test/java/com/hu/SpringbootSimpleManagerMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSimpleManagerMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
